98e7e754a713155b1d4df1633778b526a889a32a
Rogério De Oliveira <rogeriofilho632@gmail.com>
2019-12-17 15:39:34 -0300

    initial configs

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", //responsável por alteras as propriedades JS que o
+                        // browser ainda não entende.
+    "@babel/preset-react" // responsável por alterar as propriedades do React
+                          // para que seja legível para o browser.
+  ]
+
+};
